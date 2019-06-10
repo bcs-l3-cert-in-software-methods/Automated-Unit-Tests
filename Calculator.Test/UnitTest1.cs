f321862b1028da060af2781419e7f01cf913e665
Mike Chittenden <mikechittenden@mikechittenden.onmicrosoft.com>
2019-06-10 15:12:39 +0100

    Commit2

---
+++ Calculator.Test/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculator
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Add_3_and_5_should_equal_8()
+        {
+            //Arrange
+            Calculator calc = new Calculator();
+
+            //Act
+            int result = calc.Add(3, 5);
+
+            //Assert
+            Assert.AreEqual(8, result);
+        }
+    }
+}
---
+++ Test1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculator;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+
+            int result = calc.Add(3, 5);
+
+            if (result != 8)
+                throw new InvalidOperationException();
+                
+        }
+    }
+}
